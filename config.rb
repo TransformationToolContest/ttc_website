9448d444a92bd561f9e0e5a83f7e122e44949f98
Louis Rose <louis.rose@york.ac.uk>
2013-11-08 11:24:20 +0000

    Switch to deploying to Github Pages rather than Dropbox.

--- config.rb
+++ config.rb
@@ -70,3 +70,10 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
+
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.remote = "https://github.com/TransformationToolContest/TransformationToolContest.github.io.git"
+  deploy.branch = "master"
+end
--- deploy.sh
+++
@@ -1,7 +0,0 @@
-middleman build
-
-echo "Removing existing TTC website from Dropbox"
-rm -rf ~/Dropbox/Public/ttc_website/*
-
-echo "Copying new version of website to Dropbox"
-cp -R ./build/* ~/Dropbox/Public/ttc_website/
